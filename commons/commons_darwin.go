db141f507108b58aaa53febb676a2295e35ef229
杨红岩 <snxamdf@126.com>
2022-10-06 15:41:33 +0800

    README.zh-CN.md

---
+++ commons/commons_darwin.go
@@ -0,0 +1,12 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under GNU General Public License v3.0
+//
+//----------------------------------------
+
+//go:build darwin
+// +build darwin
+
+package commons
--- example/simple/Simple.go
+++ example/simple/Simple.go
@@ -17,17 +17,20 @@ func main() {
 	cefApp := cef.NewApplication(cfg)
 	//对主窗口的配置
 	cef.BrowserWindow.Config.DefaultUrl = "https://www.baidu.com"
-	cef.BrowserWindow.Config.Title = "这是一个简单示例 - 标题"
+	cef.BrowserWindow.Config.Title = "energy - 这是一个简单的窗口示例"
 	cef.BrowserWindow.Config.Width = 1024
 	cef.BrowserWindow.Config.Height = 768
 	cef.BrowserWindow.Config.SetChromiumConfig(cef.NewChromiumConfig())
 	//通过创建窗口时的回调函数 对浏览器事件设置，和窗口属性组件等创建和修改
 	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, browserWindow *cef.TCefWindowInfo) {
+		//设置应用图标
+		//lcl.Application.Icon().LoadFromFSFile("")
 		fmt.Println("SetBrowserInit")
 	})
 	//通过创建窗口之后对对主窗口的属性、组件或子创建的创建
 	cef.BrowserWindow.SetBrowserInitAfter(func(browserWindow *cef.TCefWindowInfo) {
 		fmt.Println("SetBrowserInitAfter")
 	})
+	//运行应用
 	cef.Run(cefApp)
 }
