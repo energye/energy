df12a243bf53ffbcf586ecae4bf03abd9c90f73b
杨红岩 <snxamdf@126.com>
2022-10-19 18:55:29 +0800

    demo ipc

--- example/ipc/server/Server.go
+++ example/ipc/server/Server.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/energye/energy/cef"
+	"github.com/energye/energy/ipc"
 )
 
 func main() {
-	cef.UseNetIPCChannel = false
-	cef.IPC.StartBrowserIPC()
+	ipc.UseNetIPCChannel = false
+	ipc.IPC.StartBrowserIPC()
 	var i = 0
-	cef.IPC.Browser().On("on-server", func(context cef.IIPCContext) {
+	ipc.IPC.Browser().On("on-server", func(context ipc.IIPCContext) {
 		var message = context.Arguments()
 		var data = message.GetString(0)
 		fmt.Println("on-server:", data)
 		context.Response([]byte("服务端回复:" + fmt.Sprintf("%d", i)))
-		cef.IPC.Browser().EmitChannelId("on-client", 10, message)
+		ipc.IPC.Browser().EmitChannelId("on-client", 10, message)
 		i++
 	})
 	select {}
