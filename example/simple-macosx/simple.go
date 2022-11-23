410fc0dbbb7d45ff03e9ae38ed281db3f944b672
yanghy <snxamdf@126.com>
2022-11-23 19:46:52 +0800

    demo: add simple for macosx

---
+++ example/simple-macosx/simple.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/energye/energy/cef"
+	"github.com/energye/energy/common"
+	"github.com/energye/golcl/pkgs/macapp"
+)
+
+func main() {
+	//开发环境中 MacOSX平台必须在"GlobalCEFInit"之前设置CEF
+	macapp.MacApp.IsCEF(common.IsDarwin())
+	//全局初始化 每个应用都必须调用的
+	cef.GlobalCEFInit(nil, nil)
+	//创建应用
+	cefApp := cef.NewApplication(nil)
+	//指定一个URL地址，或本地html文件目录
+	cef.BrowserWindow.Config.DefaultUrl = "https://energy.yanghy.cn"
+	//运行应用
+	cef.Run(cefApp)
+}
