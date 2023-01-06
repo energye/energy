05c48954577d999df9acff69e9a9405a2e628e2e
杨红岩 <snxamdf@126.com>
2023-01-06 19:32:46 +0800

    v2.2.3 Fixed Chinese input and gtk3 issues for linux packaging

--- example/browser-cookie/cookie.go
+++ example/browser-cookie/cookie.go
@@ -51,7 +51,7 @@ func main() {
 		})
 	})
 	//在SetBrowserInit中设置cookie事件,这些事件将返回操作后的结果
-	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, browserWindow cef.ILCLBrowserWindow) {
+	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, browserWindow cef.IBrowserWindow) {
 		//获取cookie时触发
 		event.SetOnCookiesVisited(func(sender lcl.IObject, cookie *cef.ICefCookie) {
 			fmt.Printf("SetOnCookiesVisited: %+v\n", cookie)
