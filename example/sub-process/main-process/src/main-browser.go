fb39e041e0ae54fcb63405b391656f687b8982b0
yanghy <snxamdf@126.com>
2023-02-03 19:00:43 +0800

    v2.5.3 demos: sub process start, config

--- example/sub-process/main-process/src/main-browser.go
+++ example/sub-process/main-process/src/main-browser.go
@@ -12,6 +12,7 @@ func MainBrowserInit() {
 	//指定一个URL地址，或本地html文件目录
 	cef.BrowserWindow.Config.Url = "http://localhost:22022/index.html"
 	cef.BrowserWindow.Config.IconFS = "resources/icon.ico"
+	cef.BrowserWindow.Config.Title = "ENERGY 区分主/子进程执行文件"
 	//主进程 IPC事件
 	ipc.IPC.Browser().SetOnEvent(func(event ipc.IEventOn) {
 		fmt.Println("主进程IPC事件注册")
@@ -22,7 +23,6 @@ func MainBrowserInit() {
 	//SetOnBeforePopup是子进程弹出窗口时触发，可以改变子进程窗口属性
 	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, window cef.IBrowserWindow) {
 		fmt.Println("主窗口初始化回调函数")
-		window.SetTitle("这里设置应用标题")
 		window.SetCenterWindow(true)
 
 		event.SetOnBeforePopup(func(sender lcl.IObject, browser *cef.ICefBrowser, frame *cef.ICefFrame, beforePopupInfo *cef.BeforePopupInfo, popupWindow cef.IBrowserWindow, noJavascriptAccess *bool) bool {
