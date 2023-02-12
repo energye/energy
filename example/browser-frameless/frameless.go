f63817e842c89dbe880f5deef10b8f803da69257
杨红岩 <snxamdf@126.com>
2023-02-12 18:51:01 +0800

    demos: 增加无边框无标题可拖拽窗口示例

---
+++ example/browser-frameless/frameless.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/energye/energy/cef"
+	"github.com/energye/energy/common/assetserve"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func main() {
+	//全局初始化 每个应用都必须调用的
+	cef.GlobalInit(nil, &resources)
+	//创建应用
+	cefApp := cef.NewApplication(nil)
+	//指定一个URL地址，或本地html文件目录
+	cef.BrowserWindow.Config.Url = "http://localhost:22022/index.html"
+	cef.BrowserWindow.Config.IconFS = "resources/icon.ico"
+	cef.BrowserWindow.Config.Title = "Energy 无边框可拖拽窗口"
+	cef.BrowserWindow.Config.EnableHideCaption = true
+	config := cef.BrowserWindow.Config.ChromiumConfig()
+	config.SetEnableViewSource(true)
+	cef.SetBrowserProcessStartAfterCallback(func(b bool) {
+		fmt.Println("主进程启动 创建一个内置http服务")
+		//通过内置http服务加载资源
+		server := assetserve.NewAssetsHttpServer()
+		server.PORT = 22022
+		server.AssetsFSName = "resources" //必须设置目录名
+		server.Assets = &resources
+		go server.StartHttpServer()
+	})
+	//运行应用
+	cef.Run(cefApp)
+}
