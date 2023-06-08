9f9b51a37e33ab8a1039d8cf114135eeb7e62c4b
杨红岩 <snxamdf@126.com>
2023-06-08 22:02:42 +0800

    A: TCefEventFlags CONST

--- consts/consts.go
+++ consts/consts.go
@@ -344,6 +344,28 @@ const (
 // /include/internal/cef_types.h (cef_event_flags_t)
 type TCefEventFlags = types.UInt32
 
+const (
+	EVENTFLAG_NONE                TCefEventFlags = 0
+	EVENTFLAG_CAPS_LOCK_ON                       = 1 << 0
+	EVENTFLAG_SHIFT_DOWN                         = 1 << 1
+	EVENTFLAG_CONTROL_DOWN                       = 1 << 2
+	EVENTFLAG_ALT_DOWN                           = 1 << 3
+	EVENTFLAG_LEFT_MOUSE_BUTTON                  = 1 << 4
+	EVENTFLAG_MIDDLE_MOUSE_BUTTON                = 1 << 5
+	EVENTFLAG_RIGHT_MOUSE_BUTTON                 = 1 << 6
+)
+
+// Mac OS-X command key
+const (
+	EVENTFLAG_COMMAND_DOWN TCefEventFlags = 1 << 7
+	EVENTFLAG_NUM_LOCK_ON                 = 1 << 8
+	EVENTFLAG_IS_KEY_PAD                  = 1 << 9
+	EVENTFLAG_IS_LEFT                     = 1 << 10
+	EVENTFLAG_IS_RIGHT                    = 1 << 11
+	EVENTFLAG_ALTGR_DOWN                  = 1 << 12
+	EVENTFLAG_IS_REPEAT                   = 1 << 13
+)
+
 type TCefWindowHandleType = types.Int8
 
 // /include/internal/cef_types_win.h (cef_window_handle_t)
