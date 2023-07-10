0b34c484cbec486f2c831dd6fed01870f84bf955
杨红岩 <snxamdf@126.com>
2023-07-10 14:09:50 +0800

    U: app

--- example/simple/simple.go
+++ example/simple/simple.go
@@ -8,9 +8,9 @@ func main() {
 	//全局初始化 每个应用都必须调用的
 	cef.GlobalInit(nil, nil)
 	//创建应用
-	cefApp := cef.NewApplication()
+	app := cef.NewApplication()
 	//指定一个URL地址，或本地html文件目录
 	cef.BrowserWindow.Config.Url = "https://www.baidu.com"
 	//运行应用
-	cef.Run(cefApp)
+	cef.Run(app)
 }
