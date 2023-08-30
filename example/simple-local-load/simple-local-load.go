2566569121f65d9678feaf324b108ba9f728c8aa
杨红岩 <snxamdf@126.com>
2023-08-30 19:28:56 +0800

    增加内置资源加载使用示例

--- example/simple-local-load/simple-local-load.go
+++ example/simple-local-load/simple-local-load.go
@@ -14,12 +14,13 @@ func main() {
 	// 创建应用
 	app := cef.NewApplication()
 	// 本地加载资源方式, 直接读取本地或内置执行文件资源
-	// 该模块不使用http server
+	// 该模块不使用 http server
 	// 默认访问地址fs://energy/index.html, 仅能在应用内访问
-	// fs: 默认的自定义协议名, 你可以任意设置
-	// energy: 默认的自定义域, 你可以任意设置
-	// index.html: 默认打开的页面名，你可以任意设置
-	//
+	//   fs: 默认的自定义协议名, 你可以任意设置
+	//   energy: 默认的自定义域, 你可以任意设置
+	//   index.html: 默认打开的页面名，你可以任意设置
+	// 页面ajax xhr数据获取
+	// xhr数据获取通过Proxy配置, 支持http, https证书配置
 	cef.BrowserWindow.Config.LocalResource(cef.LocalLoadConfig{
 		ResRootDir: "resources",
 		FS:         &resources,
