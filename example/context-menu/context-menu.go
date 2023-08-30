1420e668e445a7425a02da5ced4d1ffd444bd78a
zhangli10 <zhangli10@asiainfo.com>
2023-08-30 19:18:05 +0800

    增加内置资源加载使用示例

--- example/context-menu/context-menu.go
+++ example/context-menu/context-menu.go
@@ -7,7 +7,6 @@ import (
 	"github.com/energye/energy/v2/cef/ipc"
 	"github.com/energye/energy/v2/common"
 	"github.com/energye/energy/v2/consts"
-	"github.com/energye/energy/v2/pkgs/assetserve"
 	"github.com/energye/golcl/lcl"
 )
 
@@ -20,22 +19,16 @@ func main() {
 	//创建应用
 	cefApp := cef.NewApplication()
 	//指定一个URL地址，或本地html文件目录
-	cef.BrowserWindow.Config.Url = "http://localhost:22022/index.html"
 	if common.IsLinux() && cefApp.IsUIGtk3() {
 		cef.BrowserWindow.Config.IconFS = "resources/icon.png"
 	} else {
 		cef.BrowserWindow.Config.IconFS = "resources/icon.ico"
 	}
-	//主进程启动成功之后回调
-	cef.SetBrowserProcessStartAfterCallback(func(b bool) {
-		fmt.Println("主进程启动 创建一个内置http服务")
-		//通过内置http服务加载资源
-		server := assetserve.NewAssetsHttpServer()
-		server.PORT = 22022
-		server.AssetsFSName = "resources" //必须设置目录名
-		server.Assets = &resources
-		go server.StartHttpServer()
-	})
+	cef.BrowserWindow.Config.Url = "fs://energy"
+	cef.BrowserWindow.Config.LocalResource(cef.LocalLoadConfig{
+		ResRootDir: "resources",
+		FS:         &resources,
+	}.Build())
 	//在主窗口初始化监听浏览器事件
 	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, window cef.IBrowserWindow) {
 		var (
