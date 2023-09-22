512b1a469c02d71cf24d28254706f4addd893dab
zhangli10 <zhangli10@asiainfo.com>
2023-09-22 14:53:54 +0800

    command-line: darwin package

--- cmd/internal/packager/pkg_app_darwin.go
+++ cmd/internal/packager/pkg_app_darwin.go
@@ -39,6 +39,14 @@ const (
 	darwinInfoPList = "darwin/Info.plist"
 )
 
+const (
+	macCefHelper          = "Helper"
+	macCefHelperGpu       = "Helper (GPU)"
+	macCefHelperPlugin    = "Helper (Plugin)"
+	macCefHelpersRenderer = "Helper (Renderer)"
+	cef                   = "Chromium Embedded Framework.framework"
+)
+
 var (
 	pkgInfo  = []byte{0x41, 0x50, 0x50, 0x4C, 0x3F, 0x3F, 0x3F, 0x3F, 0x0D, 0x0A}
 	sipsCmds = []string{
@@ -52,6 +60,12 @@ var (
 		"-z 512 512 %s -o icons.iconset/%s_256x256@2x.png",
 		"-z 512 512 %s -o icons.iconset/%s_512x512.png",
 		"-z 1024 1024 %s -o icons.iconset/%s_512x512@2x.png",
+	}
+	helpers = []string{
+		macCefHelper,
+		macCefHelperGpu,
+		macCefHelperPlugin,
+		macCefHelpersRenderer,
 	}
 )
 
@@ -79,6 +93,20 @@ func GeneraInstaller(proj *project.Project) error {
 	if err := copyFrameworkFile(proj, appRoot); err != nil {
 		return err
 	}
+	return nil
+}
+
+func copyFrameworkFile(proj *project.Project, appRoot string) error {
+	term.Logger.Info("Generate app copy framework:",
+		term.Logger.Args("company", proj.Info.CompanyName, "product", proj.Info.ProductName))
+
+	return nil
+}
+
+func copyHelperFile(proj *project.Project, appRoot string) error {
+	term.Logger.Info("Generate app copy framework:",
+		term.Logger.Args("company", proj.Info.CompanyName, "product", proj.Info.ProductName))
+
 	return nil
 }
 
@@ -222,80 +250,5 @@ func createAppPkgInfo(proj *project.Project, appRoot string) error {
 	if err := assets.WriteFile(proj, infoPListFile, pkgInfo); err != nil {
 		return err
 	}
-	return nil
-}
-
-func copyFrameworkFile(proj *project.Project, appRoot string) error {
-	term.Logger.Info("Generate app copy framework:",
-		term.Logger.Args("company", proj.Info.CompanyName, "product", proj.Info.ProductName))
-	//buildOutDir := assets.BuildOutPath(proj)
-	//appDir := filepath.Join(buildOutDir, appRoot)
-	//contents := filepath.Join(appDir, appContents)
-	//var copyFiles = func(src, dst string) error {
-	//	if srcFile, err := os.Open(src); err != nil {
-	//		return err
-	//	} else {
-	//		st, err := srcFile.Stat()
-	//		if err != nil {
-	//			return err
-	//		}
-	//		if st.IsDir() {
-	//			srcFile.Close() //close
-	//			var pathLen = len(src)
-	//			err := filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
-	//				if path == src { // current root
-	//					return nil
-	//				}
-	//				outPath := path[pathLen:]
-	//				// exclude file or dir
-	//				for _, p := range proj.Dpkg.Exclude {
-	//					if strings.Contains(outPath, p) {
-	//						return nil
-	//					}
-	//				}
-	//				targetPath := filepath.Join(dst, outPath)
-	//				info, _ := d.Info()
-	//				if d.IsDir() {
-	//					return os.MkdirAll(targetPath, info.Mode())
-	//				} else {
-	//					if tools.IsExistAndSize(targetPath, info.Size()) {
-	//						term.Logger.Info("\tcopy skip: " + outPath)
-	//						return nil
-	//					}
-	//					srcFile, err := os.Open(path)
-	//					if err != nil {
-	//						return err
-	//					}
-	//					defer srcFile.Close()
-	//					dstFile, err := os.OpenFile(targetPath, os.O_CREATE|os.O_WRONLY, info.Mode())
-	//					if err != nil {
-	//						return err
-	//					}
-	//					defer dstFile.Close()
-	//					term.Logger.Info("\tcopy: " + outPath)
-	//					_, err = io.Copy(dstFile, srcFile)
-	//					return err
-	//				}
-	//			})
-	//			if err != nil {
-	//				return err
-	//			}
-	//		} else {
-	//			defer srcFile.Close()
-	//			dstFilePath := filepath.Join(dst, st.Name())
-	//			dstFile, err := os.OpenFile(dstFilePath, os.O_CREATE|os.O_WRONLY, st.Mode())
-	//			if err != nil {
-	//				return err
-	//			}
-	//			defer dstFile.Close()
-	//			_, err = io.Copy(dstFile, srcFile)
-	//			if err != nil {
-	//				return err
-	//			}
-	//		}
-	//	}
-	//	return nil
-	//}
-
 	return nil
 }
