227784519741c746b9910440d15d6d4fc287c03c
杨红岩 <snxamdf@126.com>
2023-11-09 18:39:59 +0800

    U: 修改构建示例测试

--- example/build-examples/build-examples.go
+++ example/build-examples/build-examples.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 const (
@@ -19,19 +20,23 @@ const (
 
 func main() {
 	wd := consts.CurrentExecuteDir
+	spDir := strings.Index(wd, "example")
+	if spDir > 0 {
+		wd = wd[:spDir]
+	}
 	println("current:", wd)
 	dist := filepath.Join(wd, "example", "dist")
 	if !isExist(dist) {
 		os.MkdirAll(dist, os.ModePerm)
 	}
-	examples := []string{"context-menu", "control", "cookie", "cookie-manager", "custom-drag-window", "dev-tools",
+	examples := []string{"audio-video", "clipbrd", "context-menu", "control-widget", "cookie", "cookie-manager", "custom-drag-window", "dev-tools",
 		"dom-visitor", "download", "drag-file", "execute-dev-tools-method", "execute-javascript", "flash-for-cef-v87",
-		"frameless", "i18n", "internal-http-server", "ipc-on-emit/go-composite-type", "ipc-on-emit/go-to-js", "ipc-on-emit/js-to-go",
-		"key-event", "lib-checkupdate", "load-html-url",
-		"msgbox", "osr/linux", "osr/windows", "popup-sub-window", "print-pdf", "process-type", "proxy", "response-filter",
-		"scheme", "screen", "search-text", "simple", "sub-process/main-process", "sub-process/sub-process", "sys-dialog",
-		"sys-menu", "tempdll", "view-source", "vue",
-		"webkit-register", "zoom", "audio-video", "clipbrd", "window/close-for-hidden", "window/close-for-minimize", "window/default-hidden-window"}
+		"frameless", "html5test", "i18n", "internal-http-server", "ipc-on-emit/go-composite-type", "ipc-on-emit/go-to-go", "ipc-on-emit/go-to-js", "ipc-on-emit/js-to-go",
+		"key-event", "lib-checkupdate", "load-html-url", "local-load", "mockevent",
+		"msgbox", "popup-sub-window", "print-pdf", "process-type", "proxy", "response-filter",
+		"scheme", "screen", "screenshot", "search-text", "simple", "simple-local-load", "sub-process/main-process", "sub-process/sub-process", "sys-dialog",
+		"sys-menu", "tempdll", "view-source", "vue", "window/close-for-hidden", "window/close-for-minimize", "window/default-hidden-window", "window/new-window",
+		"webkit-register", "window/close-for-hidden", "window/close-for-minimize", "window/default-hidden-window", "zoom"}
 	var (
 		ext     string
 		ldflags string
@@ -39,25 +44,16 @@ func main() {
 	)
 
 	if isWindows {
-		examples = append(examples, "windows/custom-browser-create", "windows/transparent")
+		examples = append(examples, "windows/custom-browser-create", "windows/notintaskbar", "windows/transparent", "osr/windows")
 		examples = append(examples, "tray/lclceftray", "tray/lcltray", "tray/systray", "tray/lclvftray")
 		ext = ".exe"
 		ldflags = `-H windowsgui -s -w`
-	} else if isLinux || isDarwin {
+	} else if isLinux {
+		examples = append(examples, "osr/linux")
+		tags = `-tags=tempdll gtk3`
+	}
+	if isLinux || isDarwin {
 		examples = append(examples, "tray/lcltray", "tray/systray")
-		var gtk string
-		if isLinux {
-			print(`Linst: Please select the GTK version to build, default GTK3
-	1. GTK2
-	2. GTK3
-Input: `)
-			fmt.Scan(&gtk)
-			if gtk == "1" {
-				tags = `-tags=tempdll gtk2`
-			} else {
-				tags = `-tags=tempdll gtk3`
-			}
-		}
 	}
 	cmd := command.NewCMD()
 	for i, example := range examples {
@@ -75,31 +71,8 @@ Input: `)
 			println()
 		}
 	}
-	println("build end.")
-	if isWindows {
-		print(`Build sample completed, do you want to use upx tool for compression
-	Need to install upx compression tool: https://github.com/upx/upx/releases
-Input 1 IS: `)
-		var useUPX string
-		fmt.Scan(&useUPX)
-		if useUPX == "1" {
-			for i, example := range examples {
-				filePath := filepath.Join(dist, example+ext)
-				if isExist(filePath) {
-					println("upx compression", example, fmt.Sprintf("%d/%d", i+1, len(examples)), "\n\tpath:", filePath)
-					cmd.Command("upx", filePath) // upx 压缩
-					println()
-				} else {
-					println("error not found:", filePath)
-					println()
-				}
-			}
-		} else {
-			println("upx exit")
-		}
-		println("upx end.")
-	}
 	cmd.Close()
+	println("build end.")
 }
 
 func isExist(path string) bool {
