5d59fd0b8eadedca9af470b5fd1bb69f4da23d08
杨红岩 <snxamdf@126.com>
2023-11-13 11:07:30 +0800

    A: 增加命令行工具MD5校验值

---
+++ cmd/energy/md5/energy-md5.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		println("args len != 2")
+		return
+	}
+	wd, _ := os.Getwd()
+	file := os.Args[1]
+	filePath := filepath.Join(wd, file)
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	hash := md5.New()
+	hash.Write(data)
+	md5String := hex.EncodeToString(hash.Sum(nil))
+	md5File, err := os.OpenFile("md5.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer md5File.Close()
+	md5File.WriteString(file + ": " + md5String + "\n")
+	println("file:", file, "md5:", md5String)
+}
