4aaab92557a6f6a0b96de2d366e4c9b3adc33b8d
杨红岩 <snxamdf@126.com>
2023-12-08 21:25:49 +0800

    A: demo IPC 多窗口通信

--- example/ipc-on-emit/multiple-window/multiplewindow.go
+++ example/ipc-on-emit/multiple-window/multiplewindow.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"github.com/energye/energy/v2/cef"
 	"github.com/energye/energy/v2/cef/ipc"
 	"github.com/energye/energy/v2/cef/ipc/callback"
 	"github.com/energye/energy/v2/cef/ipc/target"
+	"github.com/energye/golcl/lcl"
 	"time"
 )
 
@@ -17,7 +19,10 @@ func main() {
 	cef.GlobalInit(nil, &assets)
 	//创建应用
 	app := cef.NewApplication()
+	app.SetExternalMessagePump(false)
+	app.SetMultiThreadedMessageLoop(false)
 	cef.BrowserWindow.Config.Title = "Energy - ipc multiple-window"
+	//cef.BrowserWindow.Config.EnableMainWindow = false
 	//本地资源加载
 	cef.BrowserWindow.Config.LocalResource(cef.LocalLoadConfig{
 		ResRootDir: "assets",
@@ -29,11 +34,19 @@ func main() {
 	ipc.On("sendMessage", func(channel callback.IChannel) {
 		// 获得所有窗口
 		infos := cef.BrowserWindow.GetWindowInfos()
+		fmt.Println("windows-count:", len(infos))
 		for _, info := range infos {
 			// 将消息发送到目标窗口, 类型为JS接收
 			iTarget := info.Browser().Target(target.TgJs)
 			ipc.EmitTarget("receiveMessage", iTarget, time.Now().String())
 		}
+	})
+
+	cef.BrowserWindow.SetBrowserInit(func(event *cef.BrowserEvent, window cef.IBrowserWindow) {
+		event.SetOnBeforePopup(func(sender lcl.IObject, browser *cef.ICefBrowser, frame *cef.ICefFrame, beforePopupInfo *cef.BeforePopupInfo, popupWindow cef.IBrowserWindow, noJavascriptAccess *bool) bool {
+			popupWindow.SetSize(800, 600)
+			return false
+		})
 	})
 	//运行应用
 	cef.Run(app)
