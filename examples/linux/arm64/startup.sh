df194c41ea0d304079b576c9b2c9eb8700c45915
杨红岩 <snxamdf@126.com>
2024-04-25 13:39:12 +0800

    A: linux arm64 demo startup.sh

---
+++ examples/linux/arm64/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This is a solution to the CEF loading libcef.so error in the Linux ARM64 architecture
+# The key lies in: export LD_PRELOAD="/you/cef/xxx/libcef.so"
+# test: copy the shell to exe dir
+
+LIBCEF="$ENERGY_HOME/libcef.so"
+
+echo $LIBCEF
+
+# fix: linux arm: Error loading libcef.so
+# temp LD_PRELOAD, Load libcef.so correctly
+export LD_PRELOAD="$LIBCEF"
+
+# Execution File Name
+STARTUP="$PWD/$1"
+
+exec $STARTU
