8a7cedb131aa82d5aa41951058922e75740661a3
杨红岩 <snxamdf@126.com>
2024-05-29 19:40:02 +0800

    U: demo 3party load libenergy.dll

--- examples/dll/pyLoadLibenergy.py
+++ examples/dll/pyLoadLibenergy.py
@@ -1,7 +1,5 @@
 import tkinter as tk
-
 from ctypes import cdll, wintypes
-
 import os
 
 def main():
@@ -9,13 +7,14 @@ def main():
     root.title("python-load-dll")
     root.geometry("400x300")
 
-
     current_directory = os.getcwd()
     libenergyPath = current_directory + "\libenergy.dll"
     print('libenergyPath: ', libenergyPath)
 
+    # 加载dll句柄
     libenergy = cdll.LoadLibrary(libenergyPath)
 
+    # 获取energy 导出的 api
     initCEFApplication = libenergy.initCEFApplication
     cefFormShow = libenergy.cefFormShow
     cefFormFree = libenergy.cefFormFree
@@ -26,10 +25,12 @@ def main():
     showWindowBtn = tk.Button(root, text="显示libenergy创建的窗口", command=lambda: show_window_function())
     showWindowBtn.pack()
 
+    # 1.先加载dll
     def load_dll_function():
         print('call_dll_function')
         initCEFApplication()
 
+    # 2. 显示窗口
     def show_window_function():
         print('show_window_function')
         cefFormShow()
