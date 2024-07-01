acc9be05fbc4865fe1a3b8c25da3beac0a2e1849
yanghy <snxamdf@126.com>
2024-07-01 09:49:44 +0800

    v3: cef dev

--- cef/init.go
+++ cef/init.go
@@ -13,8 +13,19 @@
 package cef
 
 import (
+	"github.com/energye/cef/cef"
+	"github.com/energye/lcl/emfs"
 	"github.com/energye/lcl/process"
 )
+
+// Init 全局初始化, 需手动调用的函数
+//
+//	参数:
+//	   libs 内置到应用程序的类库
+//	   resources 内置到应用程序的资源文件
+func Init(libs emfs.IEmbedFS, resources emfs.IEmbedFS) {
+	cef.Init(libs, resources)
+}
 
 // v8init v8初始化
 func v8init() {
