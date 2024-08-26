9165104f53a26ed28ec4064c029fb03e49d90b42
杨红岩 <snxamdf@126.com>
2024-08-26 12:46:15 +0800

    v3: wv dev, local load

--- wv/local_load.go
+++ wv/local_load.go
@@ -25,14 +25,14 @@ const (
 //
 //	Respond directly by loading local or built-in resources
 type LocalLoad struct {
-	Scheme string // 自定义协议, 不建议使用 HTTP、HTTPS、FILE、FTP、ABOUT和DATA 默认: fs
-	Domain string // 自定义域, 格式: xxx | xxx.xx | xxx.xxx.xxx， example, example.com, 默认: energy
-	// 资源根目录, fs为空时: 本地目录(默认当前程序执行目录), fs不为空时: 默认值 resources, 使用内置加载
-	// 本地目录规则: 空("")时当前目录, @当前目录开始(@/to/path)，或绝对目录.
-	ResRootDir string        //
-	FS         emfs.IEmbedFS // 内置加载资源对象, 不为nil时使用内置加载，默认: nil
-	Proxy      IXHRProxy     // 数据请求代理, 在浏览器发送xhr请求时可通过该配置转发, 你可自定义实现该 IXHRProxy 接口
-	Home       string        // 默认首页HTML文件名: /index.html , 默认: /index.html
+	Scheme string // User-defined protocol default: fs
+	Domain string // The format is xxx | xxx.xx | xxx.xxx.xxx, example, example.com. default: energy
+	// When fs is empty: local directory (default current program execution directory), when fs is not empty: default resources, using built-in load
+	// Local directory rules: current directory when empty (""), @start of current directory (@/to/path), or absolute directory.
+	ResRootDir string        // Resource root directory
+	FS         emfs.IEmbedFS // Built-in load resource object, use built-in load when not nil, default: nil
+	Proxy      IXHRProxy     // Proxy, which can be forwarded through this configuration when the browser sends xhr requests, you can customize the implementation of the IXHRProxy interface
+	Home       string        // Home resources, default: /index.html
 	exePath    string
 }
 
@@ -49,7 +49,7 @@ func (m *LocalLoad) defaultInit() {
 		m.Home = "/" + m.Home
 	}
 	m.exePath = exec.Path
-	// 默认的资源目录
+	// default resource dir
 	if m.ResRootDir == "" {
 		if m.FS != nil && m.ResRootDir == "" {
 			// embed exe
