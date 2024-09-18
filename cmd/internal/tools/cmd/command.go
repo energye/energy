f3c24e34d93832df61699e56574bd6427bd8e907
杨红岩 <snxamdf@126.com>
2024-09-18 21:35:35 +0800

    U: energy cli, separate mod module independently

---
+++ cmd/internal/tools/cmd/command.go
@@ -0,0 +1,101 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License 2.0
+//
+//----------------------------------------
+
+package command
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+type CMD struct {
+	HideWindow      bool
+	IsPrint         bool
+	Dir             string
+	MessageCallback func([]byte, error)
+	stdout          io.ReadCloser
+}
+
+func NewCMD() *CMD {
+	return &CMD{IsPrint: true}
+}
+
+func IsWindows() bool {
+	return runtime.GOOS == "windows"
+}
+
+func (m *CMD) Close() {
+	if m.stdout != nil {
+		m.stdout.Close()
+	}
+}
+
+func (m *CMD) Command(name string, args ...string) {
+	if m.IsPrint {
+		fmt.Println("command name:", name, "args:", args)
+	}
+	cmd := exec.Command(name, args...)
+	if m.Dir != "" {
+		cmd.Dir = m.Dir
+	}
+	//隐藏调用外部命令窗口
+	if m.HideWindow && IsWindows() {
+		cmd.SysProcAttr = HideWindow(true)
+	}
+	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		if m.MessageCallback != nil {
+			m.MessageCallback(nil, err)
+		}
+		return
+	}
+	m.stdout = stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		if m.MessageCallback != nil {
+			m.MessageCallback(nil, err)
+		}
+		return
+	}
+	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+	reader := bufio.NewReader(stdout)
+	//实时循环读取输出流中的一行内容
+	for {
+		byt, b, err := reader.ReadLine()
+		if err != nil || io.EOF == err {
+			if m.MessageCallback != nil {
+				m.MessageCallback([]byte("exit"), nil)
+			}
+			break
+		}
+		if m.MessageCallback != nil {
+			m.MessageCallback(byt, nil)
+		} else {
+			if m.IsPrint {
+				fmt.Println("line:", string(byt), b)
+			}
+		}
+	}
+	err = cmd.Wait()
+	if err != nil {
+		if m.MessageCallback != nil {
+			m.MessageCallback(nil, errors.New("wait end error "+err.Error()))
+		}
+	} else {
+		if m.MessageCallback != nil {
+			m.MessageCallback(nil, nil)
+		}
+	}
+}
---
+++ cmd/internal/tools/winicon/generate.go
@@ -0,0 +1,102 @@
+package winicon
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+
+	"github.com/energye/energy/v2/cmd/internal/tools/winicon/internal/winicon"
+	"golang.org/x/image/draw"
+)
+
+// GenerateIcon reads image data from the given reader and generates
+// a .ico file that is written to the given writer. The .ico file will include
+// a number of icons at the sizes given.
+func GenerateIcon(r io.Reader, w io.Writer, sizes []int) error {
+	header := &winicon.IconFileHeader{
+		ImageType:  1,
+		ImageCount: uint16(len(sizes)),
+	}
+
+	iconheaders := make([]winicon.IconHeader, len(sizes))
+
+	var imageData bytes.Buffer
+
+	// Decode to internal image
+	imagedata, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+
+	// Loop over sizes desired
+	for index, size := range sizes {
+
+		// Check target size
+		if size == 0 {
+			return fmt.Errorf("a size of 0 is not valid")
+		}
+
+		// Scale image
+		rect := image.Rect(0, 0, size, size)
+		rawdata := image.NewRGBA(rect)
+		scale := draw.CatmullRom
+		scale.Scale(rawdata, rect, imagedata, imagedata.Bounds(), draw.Over, nil)
+
+		// Convert back to PNG
+		icondata := new(bytes.Buffer)
+		writer := bufio.NewWriter(icondata)
+		err = png.Encode(writer, rawdata)
+		if err != nil {
+			return err
+		}
+		err = writer.Flush()
+		if err != nil {
+			return err
+		}
+
+		// Save image data
+		imageData.Write(icondata.Bytes())
+
+		// Save header information
+		if size >= 256 {
+			size = 0
+		}
+		iconheaders[index].Width = (uint8)(size)
+		iconheaders[index].Height = (uint8)(size)
+		iconheaders[index].BitsPerPixel = 32
+		iconheaders[index].Size = uint32(len(icondata.Bytes()))
+	}
+
+	// Update the offsets. Start by skipping header+icon headers
+	var currentOffset uint32 = (uint32)(6 + (16 * len(iconheaders)))
+
+	for index := range iconheaders {
+		iconheaders[index].Offset = currentOffset
+		currentOffset += iconheaders[index].Size
+	}
+
+	// Write out the header
+	err = binary.Write(w, binary.LittleEndian, header)
+	if err != nil {
+		return err
+	}
+
+	// Write out the icon headers
+	for _, iconheader := range iconheaders {
+		err = binary.Write(w, binary.LittleEndian, iconheader)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Write out the image data
+	_, err = w.Write(imageData.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
