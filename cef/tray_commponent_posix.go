662c9d809f17c1970630d5d70ebeb1b87307050f
yanghy <snxamdf@126.com>
2024-11-25 10:54:47 +0800

    Upgrades CEF 130.1.16

---
+++ cef/tray_commponent_posix.go
@@ -0,0 +1,22 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+//go:build !windows
+// +build !windows
+
+package cef
+
+// ViewsFrameTray TODO no support posix
+type ViewsFrameTray struct {
+}
+
+// CEFTray TODO no support posix
+type CEFTray struct {
+}
