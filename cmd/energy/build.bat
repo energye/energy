34a0af3e2e9690b7dcc520a79063cb5e865c1822
yanghy <snxamdf@126.com>
2024-12-10 18:13:47 +0800

    U: workflows gen cli

--- cmd/energy/build.bat
+++ cmd/energy/build.bat
@@ -20,6 +20,11 @@ set GOOS=darwin
 go build -trimpath -ldflags "-s -w" -o energy-darwinarm-64 energy.go
 md5.exe energy-darwinarm-64
 
+set GOARCH=386
+set GOOS=linux
+go build -trimpath -ldflags "-s -w" -o energy-linux-32 energy.go
+md5.exe energy-linux-32
+
 set GOARCH=amd64
 set GOOS=linux
 go build -trimpath -ldflags "-s -w" -o energy-linux-64 energy.go
@@ -29,5 +34,10 @@ set GOARCH=arm64
 set GOOS=linux
 go build -trimpath -ldflags "-s -w" -o energy-linuxarm-64 energy.go
 md5.exe energy-linuxarm-64
+
+set GOARCH=arm
+set GOOS=linux
+go build -trimpath -ldflags "-s -w" -o energy-linuxarm energy.go
+md5.exe energy-linuxarm
 
 pause
