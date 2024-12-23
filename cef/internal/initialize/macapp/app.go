e2329d986125c2ecdf060bbc1e37f4599d455a38
zhangli10 <zhangli10@asiainfo.com>
2024-12-23 17:49:10 +0800

    U: macos dev auto gen xxx.app

---
+++ cef/internal/initialize/macapp/app.go
@@ -0,0 +1,28 @@
+//go:build darwin && !prod
+// +build darwin,!prod
+
+package macapp
+
+// MacApp
+//
+//  1. macos调式时临时创建一个符合macapp的程序包
+//
+//  2. 如果基于cef，需要指定cef frameworks 根目录【/homt/xxx/cef_binary_xxxxxxx_macosx64/Release】
+var MacApp = &macApp{}
+
+type macApp struct {
+	execName             string
+	execFile             string
+	macContentsDir       string
+	macOSDir             string
+	macResources         string
+	lclLibFileName       string
+	plistFileName        string
+	pkgInfoFileName      string
+	macAppFrameworksDir  string
+	isLinked             bool
+	isMain               bool
+	cefFrameworksDir     string
+	browseSubprocessPath string
+	lsUIElement          string
+}
