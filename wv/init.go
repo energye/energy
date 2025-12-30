d82d3d2b41a8e09f72baa8ebef7ae67b8784775b
yanghy <snxamdf@126.com>
2025-12-30 16:20:34 +0800

    energy webview

--- wv/init.go
+++ wv/init.go
@@ -11,15 +11,10 @@
 package wv
 
 import (
-	"github.com/energye/lcl/emfs"
 	wv "github.com/energye/wv/windows"
 )
 
 // Init 全局初始化, 需手动调用的函数
-//
-//	参数:
-//	   libs 内置到应用程序的类库
-//	   resources 内置到应用程序的资源文件
-func Init(libs emfs.IEmbedFS, resources emfs.IEmbedFS) {
-	wv.Init(libs, resources)
+func Init() {
+	wv.Init()
 }
