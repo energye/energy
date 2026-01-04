3d903fbc6f2a3a7333a9f038670439bb311c619c
yanghy <snxamdf@126.com>
2026-01-04 18:41:57 +0800

    energy webview

--- wv/wv.go
+++ wv/wv.go
@@ -12,12 +12,17 @@ package wv
 
 import (
 	engLCL "github.com/energye/energy/v3/lcl"
+	"github.com/energye/lcl/api"
 	"github.com/energye/lcl/lcl"
+	wv "github.com/energye/wv/windows"
 )
 
 func Run(forms ...lcl.IEngForm) {
 	if gApplication != nil {
 		gApplication.Start()
 	}
+	api.SetOnReleaseCallback(func() {
+		wv.DestroyGlobalWebView2Loader()
+	})
 	engLCL.Run(forms...)
 }
