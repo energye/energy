7950f1a48480004f456ca8adf29b866011dcdd88
yanghy <snxamdf@126.com>
2026-01-24 21:28:16 +0800

    energy webkit2

---
+++ wv/wkwebview/ns_dragginginfo.go
@@ -0,0 +1,75 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+//go:build darwin
+
+package wkwebview
+
+/*
+#cgo darwin CFLAGS: -DDARWIN -x objective-c
+#cgo darwin LDFLAGS: -framework WebKit -framework Cocoa
+
+#import <WebKit/WebKit.h>
+#import <Cocoa/Cocoa.h>
+
+NSPasteboard* DraggingPasteboard(void* nsDraggingInfo) {
+	id<NSDraggingInfo> dragInfo = (id<NSDraggingInfo>)nsDraggingInfo;
+	if (!dragInfo) {
+        NSLog(@"DraggingPasteboard dragInfo is nil");
+        return nil;
+    }
+    return [dragInfo draggingPasteboard];
+}
+
+*/
+import "C"
+
+import (
+	"github.com/energye/wv/darwin"
+	"unsafe"
+)
+
+// NSDragOperation A group of constants that represent which operations the dragging source can perform on dragging items.
+type NSDragOperation int32
+
+const (
+	// NSDragOperationCopy A constant that indicates the drag can copy the data that the image represents.
+	NSDragOperationCopy = NSDragOperation(C.NSDragOperationCopy)
+	// NSDragOperationLink A constant that indicates the drag can share the data.
+	NSDragOperationLink = NSDragOperation(C.NSDragOperationLink)
+	// NSDragOperationGeneric A constant that indicates the destination can define the drag operation.
+	NSDragOperationGeneric = NSDragOperation(C.NSDragOperationGeneric)
+	// NSDragOperationPrivate A constant that indicates the source and destination negotiate the drag operation privately.
+	NSDragOperationPrivate = NSDragOperation(C.NSDragOperationPrivate)
+	// NSDragOperationMove A constant that indicates the drag can move the data.
+	NSDragOperationMove = NSDragOperation(C.NSDragOperationMove)
+	// NSDragOperationDelete A constant that indicates the drag can delete the data.
+	NSDragOperationDelete = NSDragOperation(C.NSDragOperationDelete)
+	// NSDragOperationEvery A constant that indicates that drag can perform all of the drag operations.
+	NSDragOperationEvery = NSDragOperation(C.NSDragOperationEvery)
+	// NSDragOperationNone A constant that indicates that the drag cannot perform any operations.
+	NSDragOperationNone = NSDragOperation(C.NSDragOperationNone)
+)
+
+type TNSDraggingInfo struct {
+	data unsafe.Pointer
+}
+
+func WrapNSDraggingInfo(data darwin.NSDraggingInfoProtocol) *TNSDraggingInfo {
+	return &TNSDraggingInfo{data: unsafe.Pointer(data)}
+}
+
+func (m *TNSDraggingInfo) DraggingPasteboard() *TNSPasteboard {
+	if m.data == nil {
+		return nil
+	}
+	cResult := C.DraggingPasteboard(m.data)
+	return WrapNSPasteboard(uintptr(unsafe.Pointer(cResult)))
+}
---
+++ wv/wkwebview/ns_pasteboard.go
@@ -0,0 +1,167 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+//go:build darwin
+
+package wkwebview
+
+/*
+#cgo darwin CFLAGS: -DDARWIN -x objective-c
+#cgo darwin LDFLAGS: -framework WebKit -framework Cocoa
+
+#import <WebKit/WebKit.h>
+#import <Cocoa/Cocoa.h>
+
+// 粘贴板数据结构体（Go侧对应）
+typedef struct {
+    const char* filePathsJSON; // 文件路径数组JSON
+    const char* webURLsJSON;   // 网页URL数组JSON
+    const char* textsJSON;     // 文本数组JSON
+    const void* imageData;     // 图片二进制数据
+    NSUInteger imageLength;    // 图片数据长度
+} PasteboardData;
+
+// 通用读取粘贴板数据
+PasteboardData ReadPasteboardData(void* nsPasteboard) {
+    PasteboardData data = {NULL, NULL, NULL, NULL, 0};
+    @autoreleasepool {
+        NSPasteboard* pboard = (NSPasteboard*)nsPasteboard;
+        if (!pboard) return data;
+
+        // 读取文件/URL
+        NSArray<Class>* urlClasses = @[[NSURL class]];
+        NSArray<NSURL*>* allURLs = [pboard readObjectsForClasses:urlClasses options:@{}];
+        NSMutableArray<NSString*>* filePaths = [NSMutableArray array];
+        NSMutableArray<NSString*>* webURLs = [NSMutableArray array];
+
+        for (NSURL* url in allURLs) {
+            if ([url isFileURL]) {
+                const char* fsPath = [url fileSystemRepresentation];
+                NSString* pathStr = [[NSString alloc] initWithCString:fsPath encoding:NSUTF8StringEncoding];
+                [filePaths addObject:pathStr];
+            } else {
+                [webURLs addObject:url.absoluteString];
+            }
+        }
+
+        // 序列化文件路径/URL为JSON
+        if (filePaths.count > 0) {
+            NSData* fpJSON = [NSJSONSerialization dataWithJSONObject:filePaths options:0 error:nil];
+            data.filePathsJSON = [[[NSString alloc] initWithData:fpJSON encoding:NSUTF8StringEncoding] UTF8String];
+        }
+        if (webURLs.count > 0) {
+            NSData* wuJSON = [NSJSONSerialization dataWithJSONObject:webURLs options:0 error:nil];
+            data.webURLsJSON = [[[NSString alloc] initWithData:wuJSON encoding:NSUTF8StringEncoding] UTF8String];
+        }
+
+        // 读取文本
+        NSArray<Class>* stringClasses = @[[NSString class]];
+        NSArray<NSString*>* texts = [pboard readObjectsForClasses:stringClasses options:@{}];
+        if (texts.count > 0) {
+            NSData* textJSON = [NSJSONSerialization dataWithJSONObject:texts options:0 error:nil];
+            data.textsJSON = [[[NSString alloc] initWithData:textJSON encoding:NSUTF8StringEncoding] UTF8String];
+        }
+
+        // 读取图片数据
+        NSData* imageData = [pboard dataForType:@"public.png"];
+        if (!imageData) imageData = [pboard dataForType:NSPasteboardTypeTIFF];
+        if (imageData) {
+            data.imageData = imageData.bytes;
+            data.imageLength = imageData.length;
+        }
+    }
+    return data;
+}
+
+const char* PasteboardTypes(void* nsPasteboard) {
+	NSPasteboard* pasteboard = (NSPasteboard*)nsPasteboard;
+	if (!pasteboard) {
+        NSLog(@"PasteboardTypes pasteboard is nil");
+        return NULL;
+    }
+    NSArray<NSPasteboardType> *types = [pasteboard types];
+    if (!types || types.count == 0) {
+        return NULL;
+    }
+    @autoreleasepool {
+		NSError *error = nil;
+		NSData *jsonData = [NSJSONSerialization dataWithJSONObject:types options:0 error:&error];
+		if (error || !jsonData) return NULL;
+		NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+		return [jsonStr UTF8String];
+    }
+}
+
+*/
+import "C"
+import (
+	"encoding/json"
+	"unsafe"
+)
+
+type TNSPasteboard struct {
+	data unsafe.Pointer
+}
+
+func WrapNSPasteboard(data uintptr) *TNSPasteboard {
+	return &TNSPasteboard{data: unsafe.Pointer(data)}
+}
+
+func (m *TNSPasteboard) Types() []string {
+	if m.data == nil {
+		return nil
+	}
+	cData := C.PasteboardTypes(m.data)
+	if cData == nil {
+		return nil
+	}
+	data := C.GoString(cData)
+	var result []string
+	if err := json.Unmarshal([]byte(data), &result); err != nil {
+		return nil
+	}
+	return result
+}
+
+type PasteboardData struct {
+	FilePaths []string
+	WebURLs   []string
+	Texts     []string
+	ImageData []byte
+}
+
+func (m *TNSPasteboard) PasteboardData() *PasteboardData {
+	if m.data == nil {
+		return nil
+	}
+	cData := C.ReadPasteboardData(m.data)
+
+	result := &PasteboardData{}
+	// 解析文件路径
+	if cData.filePathsJSON != nil {
+		fpJSON := C.GoString(cData.filePathsJSON)
+		_ = json.Unmarshal([]byte(fpJSON), &result.FilePaths)
+	}
+	// 解析网页URL
+	if cData.webURLsJSON != nil {
+		wuJSON := C.GoString(cData.webURLsJSON)
+		_ = json.Unmarshal([]byte(wuJSON), &result.WebURLs)
+	}
+	// 解析文本
+	if cData.textsJSON != nil {
+		textJSON := C.GoString(cData.textsJSON)
+		_ = json.Unmarshal([]byte(textJSON), &result.Texts)
+	}
+	// 解析图片数据
+	if cData.imageData != nil && cData.imageLength > 0 {
+		result.ImageData = C.GoBytes(cData.imageData, C.int(cData.imageLength))
+	}
+	return result
+}
