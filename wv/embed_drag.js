07baf3efb3c2a529795ab0f6530accda27557a43
yanghy <snxamdf@126.com>
2026-02-06 21:59:14 +0800

    energy webview2

--- wv/embed_drag.js
+++ wv/embed_drag.js
@@ -1,7 +1,12 @@
-// console.log("postMessageWithAdditionalObjects:",window.chrome.webview.postMessageWithAdditionalObjects)
-// if (window.chrome.webview.postMessageWithAdditionalObjects) {
-//     window.chrome.webview.postMessageWithAdditionalObjects(`file:drop:1:2`, dragResult.files);
-// }
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
 
 (function () {
     const MT_DRAG_DROP_ENTER = 100;
@@ -12,11 +17,10 @@
     const DragTypeFile = DragTypeNo + 1; // file list
     const DragTypeData = DragTypeFile + 1;
 
-
     const docElement = document.documentElement;
     const formats = ['text/plain', 'Files'];
-    let dragEnterCount = 0;
-    let isDragging = false;
+    let dragCount = 0;
+    let isDrag = false;
 
     const processMessage = window?.energy?.processMessage;
     const postMessageWithAdditionalObjects = function (message, additionalObjects) {
@@ -49,46 +53,46 @@
         return null;
     }
 
-    // 1. 拖拽进入事件
+    // 拖拽进入事件
     docElement.addEventListener('dragenter', (event) => {
         const format = isFormats(event);
         if (format == null) {
             return;
         }
-        dragEnterCount++;
-        if (dragEnterCount === 1 && !isDragging) {
-            isDragging = true;
+        dragCount++;
+        if (dragCount === 1 && !isDrag) {
+            isDrag = true;
             const data = {type: format.type, x: event.clientX, y: event.clientY};
             const payload = makePayload(MT_DRAG_DROP_ENTER, "", data, 0);
             processMessage(payload);
         }
     }, {passive: true});
 
-    // 2. 拖拽离开事件
+    // 拖拽离开事件
     docElement.addEventListener('dragleave', (event) => {
         const format = isFormats(event);
         if (format == null) {
             return;
         }
-        dragEnterCount--;
-        if (dragEnterCount <= 0 && isDragging) {
-            dragEnterCount = 0;
-            isDragging = false;
+        dragCount--;
+        if (dragCount <= 0 && isDrag) {
+            dragCount = 0;
+            isDrag = false;
             const data = {type: format.type, x: event.clientX, y: event.clientY};
             const payload = makePayload(MT_DRAG_DROP_LEAVE, "", data, 0);
             processMessage(payload);
         }
     }, {passive: true});
 
-    // 3. 拖拽结束事件（包含 放置完成 和 未放置直接离开）
+    // 拖拽结束事件（包含 放置完成 和 未放置直接离开）
     docElement.addEventListener('drop', (event) => {
         const format = isFormats(event);
         if (format == null) {
             return;
         }
-        if (isDragging) {
-            dragEnterCount = 0;
-            isDragging = false;
+        if (isDrag) {
+            dragCount = 0;
+            isDrag = false;
             const data = {type: format.type, x: event.clientX, y: event.clientY, text: ""};
             const dataTransfer = event.dataTransfer;
             if (format.type === DragTypeFile) {
