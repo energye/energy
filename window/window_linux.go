e478c43d00aa4bb20a38bfdda7dfaf82e15852c1
yanghy <snxamdf@126.com>
2026-04-08 21:44:18 +0800

    可使用或禁用CGO编译

--- window/window_linux.go
+++ window/window_linux.go
@@ -13,8 +13,8 @@
 package window
 
 import (
-	"github.com/energye/energy/v3/pkgs/gtk3"
-	gtk3Types "github.com/energye/energy/v3/pkgs/gtk3/types"
+	"github.com/energye/energy/v3/pkgs/linux/gtk3"
+	gtk3Types "github.com/energye/energy/v3/pkgs/linux/gtk3/types"
 	"github.com/energye/lcl/lcl"
 	"github.com/energye/lcl/types"
 	"unsafe"
