99868cc2fd6c178e6260fe1846cff281baac30cf
yanghy <snxamdf@126.com>
2026-06-05 19:29:58 +0800

    v3.0 Add and Fix: CEF for energy

---
+++ cef/app.go
@@ -0,0 +1,72 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+package cef
+
+import (
+	"github.com/energye/cef/cef"
+	"github.com/energye/cef/config"
+	"github.com/energye/lcl/tool"
+	"github.com/energye/lcl/tool/exec"
+	"path/filepath"
+)
+
+var (
+	// GApplication global application instance
+	GApplication *Application
+)
+
+type Application struct {
+	cef.ICefApplication
+}
+
+func NewApplication() *Application {
+	if GApplication == nil {
+		GApplication = &Application{
+			ICefApplication: cef.NewApplication(),
+		}
+		cef.SetGlobalCEFApplication(GApplication)
+	}
+	if !tool.IsDarwin() {
+		// Set up CEF Framework
+		// Non-macOS platforms require specifying the CEF framework directory;
+		// no configuration is needed if the executable is located inside the CEF directory.
+		cfg := config.GConfig
+		if cfg != nil {
+			libCef := func() string {
+				if tool.IsWindows() {
+					return "libcef.dll"
+				} else if tool.IsLinux() {
+					return "libcef.so"
+				}
+				return ""
+			}()
+			if libCef != "" {
+				if frameworkDir := cfg.ChromiumPath(); tool.IsExist(filepath.Join(frameworkDir, libCef)) {
+					GApplication.SetCEFFrameworkDir(frameworkDir)
+					return GApplication
+				}
+				execDir := exec.AppDir()
+				if tool.IsExist(filepath.Join(execDir, libCef)) {
+					GApplication.SetCEFFrameworkDir(execDir)
+					return GApplication
+				}
+			}
+		}
+	}
+	return GApplication
+}
+
+// SetCEFFrameworkDir Set unified key paths for CEF framework
+func (m *Application) SetCEFFrameworkDir(path string) {
+	m.ICefApplication.SetFrameworkDirPath(path)
+	m.ICefApplication.SetResourcesDirPath(path)
+	m.ICefApplication.SetLocalesDirPath(filepath.Join(path, "locales"))
+}
