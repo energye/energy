1dd11fb203c847d45b107b958719b34b8f082621
yanghy <snxamdf@126.com>
2026-06-27 20:29:38 +0800

    v3.0: Add CEF for energy

--- cef/browser_views_events.go
+++ cef/browser_views_events.go
@@ -28,6 +28,16 @@ func (m *TViewsBrowser) initViewsBrowserDefaultEvent() {
 	m.chromium.SetOnClose(m.chromiumOnClose)
 }
 
+func (m *TViewsBrowser) initViewsWindowDefaultEvent() {
+	logger.Debug("Browser.initViewsWindowDefaultEvent")
+	m.window.SetOnWindowCreated(m.windowOnWindowCreated)
+	m.window.SetOnGetInitialShowState(m.windowOnGetInitialShowState)
+	m.window.SetOnIsFrameless(m.windowOnIsFrameless)
+	m.window.SetOnGetInitialBounds(m.windowOnGetInitialBounds)
+	m.window.SetOnCanClose(m.windowOnCanClose)
+	m.window.SetOnWindowClosing(m.windowOnWindowClosing)
+}
+
 func (m *TViewsBrowser) chromiumOnAfterCreated(sender lcl.IObject, browser cef.ICefBrowser) {
 	logger.Debug("Chromium.OnAfterCreated", browser.GetIdentifier())
 	if m.browserId == 0 {
@@ -59,4 +69,90 @@ func (m *TViewsBrowser) chromiumOnBeforeClose(sender lcl.IObject, browser cef.IC
 	lcl.RunOnMainThreadAsync(func(id uint32) {
 		m.window.Close()
 	})
+}
+
+func (m *TViewsBrowser) windowOnWindowCreated(sender lcl.IObject, window cef.ICefWindow) {
+	logger.Debug("Window.OnWindowCreated")
+	if m.created {
+		return
+	}
+	m.created = true
+	lcl.CallFormCreate(m.self, m)
+
+	m.UpdateBrowserOptions()
+	url := m.defaultURL
+	if url == "" {
+		url = "about:blank"
+	}
+
+	created := m.chromium.CreateBrowserWithStrBVComponentRContextDValue(url, m.browserView, m.context, m.extraInfo)
+	logger.Debug("Window.OnWindowCreated chromium.CreateBrowser:", created)
+	if !created {
+		return
+	}
+
+	m.window.SetToFillLayout()
+	browserView := m.browserView.BrowserView()
+	if browserView != nil && browserView.IsValid() {
+		m.window.AddChildView(browserView)
+	}
+
+	if m.options != nil {
+		if m.options.Caption != "" {
+			m.window.SetTitle(m.options.Caption)
+		}
+		if m.options.Width > 0 && m.options.Height > 0 {
+			m.window.CenterWindow(cef.TCefSize{
+				Width:  m.options.Width,
+				Height: m.options.Height,
+			})
+		}
+	}
+
+	m.browserView.RequestFocus()
+	m.window.Show()
+}
+
+func (m *TViewsBrowser) windowOnGetInitialBounds(sender lcl.IObject, window cef.ICefWindow, result *cef.TCefRect) {
+	if m.options == nil || result == nil {
+		return
+	}
+	if m.options.X > 0 {
+		result.X = m.options.X
+	}
+	if m.options.Y > 0 {
+		result.Y = m.options.Y
+	}
+	if m.options.Width > 0 {
+		result.Width = m.options.Width
+	}
+	if m.options.Height > 0 {
+		result.Height = m.options.Height
+	}
+}
+
+func (m *TViewsBrowser) windowOnGetInitialShowState(sender lcl.IObject, window cef.ICefWindow, result *cefTypes.TCefShowState) {
+	logger.Debug("Window.OnGetInitialShowState")
+}
+
+func (m *TViewsBrowser) windowOnWindowClosing(sender lcl.IObject, window cef.ICefWindow) {
+	logger.Debug("Window.OnWindowClosing")
+}
+
+func (m *TViewsBrowser) windowOnIsFrameless(sender lcl.IObject, window cef.ICefWindow, result *bool) {
+	logger.Debug("Window.OnIsFrameless")
+	if m.options != nil {
+		*result = m.options.Frameless
+	}
+}
+
+func (m *TViewsBrowser) windowOnCanClose(sender lcl.IObject, window cef.ICefWindow, result *bool) {
+	logger.Debug("Window.OnCanClose")
+	if result == nil {
+		return
+	}
+	*result = m.canClose
+	if !m.canClose && m.chromium != nil {
+		m.chromium.CloseBrowser(true)
+	}
 }
